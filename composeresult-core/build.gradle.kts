c0f5706cfe243c0e207e9d377b2c568c2e0cfe1d
Lans <wylans@qq.com>
2026-03-18 10:35:13 +0800

    change version 1.0.5

--- composeresult-core/build.gradle.kts
+++ composeresult-core/build.gradle.kts
@@ -41,12 +41,12 @@ android {
 //
 //    发布到 maven { url = uri("https://jitpack.io") }
 //
-//    publishing {
-//        singleVariant("release") {
-//            withSourcesJar()
-//            withJavadocJar()
-//        }
-//    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 
 
 }
@@ -74,37 +74,37 @@ dependencies {
 //}
 
 // 发布到 mavenCentral()
-mavenPublishing {
-    coordinates(group.toString(), "compose-result", version.toString())
-
-    pom {
-        name = "ComposeResult"
-        description = "ComposeResult 是一个专为 Jetpack Compose 打造的轻量级、响应式页面通信库。它利用 CompositionLocal 和 rememberSaveable 实现了跨页面的状态共享与结果回传，优雅地取代了繁琐的 SavedStateHandle 方案。"
-        url = "https://github.com/Lans/ComposeResult"
-
-        // 开源协议配置，可根据需要修改
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-
-        developers {
-            developer {
-                id = "lans"
-                name = "lans"
-                email = "wylans@163.com"
-            }
-        }
-
-        scm {
-            connection = "scm:git:github.com/Lans/ComposeResult.git"
-            developerConnection = "scm:git:ssh://github.com/Lans/ComposeResult.git"
-            url = "https://github.com/Lans/ComposeResult"
-        }
-    }
-
-    publishToMavenCentral()
-    signAllPublications()
-}
+//mavenPublishing {
+//    coordinates(group.toString(), "compose-result", version.toString())
+//
+//    pom {
+//        name = "ComposeResult"
+//        description = "ComposeResult 是一个专为 Jetpack Compose 打造的轻量级、响应式页面通信库。它利用 CompositionLocal 和 rememberSaveable 实现了跨页面的状态共享与结果回传，优雅地取代了繁琐的 SavedStateHandle 方案。"
+//        url = "https://github.com/Lans/ComposeResult"
+//
+//        // 开源协议配置，可根据需要修改
+//        licenses {
+//            license {
+//                name = "The Apache License, Version 2.0"
+//                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+//            }
+//        }
+//
+//        developers {
+//            developer {
+//                id = "lans"
+//                name = "lans"
+//                email = "wylans@163.com"
+//            }
+//        }
+//
+//        scm {
+//            connection = "scm:git:github.com/Lans/ComposeResult.git"
+//            developerConnection = "scm:git:ssh://github.com/Lans/ComposeResult.git"
+//            url = "https://github.com/Lans/ComposeResult"
+//        }
+//    }
+//
+//    publishToMavenCentral()
+//    signAllPublications()
+//}
